17d8cd5fbf084dacaa2c20f9e9e217b5deb07e91
Moniqueemh <mohernandez9992@yahoo.com>
2021-01-30 02:36:22 -0800

    added pictures/gif to slides

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const App = () => {
     <div>
       <MyNavbar />
       <MyCarousel />
-      My Portfolio
     </div>
   );
 }
--- src/components/my-carousel/mycarousel.component.jsx
+++ src/components/my-carousel/mycarousel.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import Slide1 from '../../assets/img/slide1.jpg';
+import Slide1 from '../../assets/img/slide1.gif';
 import Slide2 from '../../assets/img/slide2.jpg';
 import Slide3 from '../../assets/img/slide3.jpg';
 
@@ -8,45 +8,31 @@ import './mycarousel.css';
 
 const MyCarousel = () => {
     return (
-        <>
-            <Carousel>
+        <div id="home">
+            <Carousel controls={false} indicators interval={2500} pause={false}>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="d-block w-100 custom-img"
                         src={Slide1}
                         alt="First slide"
                     />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="d-block w-100 custom-img"
                         src={Slide2}
                         alt="Second slide"
                     />
-
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="d-block w-100 custom-img"
                         src={Slide3}
                         alt="Third slide"
                     />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-        </>
+        </div>
     )
 }
 
