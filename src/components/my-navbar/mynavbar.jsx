0851ab4f8b126a96069d397f09670d2937fa10dc
Moniqueemh <mohernandez9992@yahoo.com>
2021-01-30 17:58:02 -0800

    finished about me page

--- src/components/my-navbar/mynavbar.jsx
+++ src/components/my-navbar/mynavbar.jsx
@@ -7,14 +7,14 @@ const MyNavbar = () => {
     return (
         <>
             <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home"><i class="fas fa-coffee"></i></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#experience">Experience</Nav.Link>
+                        {/* For Later add these links: <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#experience">Experience</Nav.Link>*/}
                         <Nav.Link href="#projects">Projects</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
