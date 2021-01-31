2261a687c263db5d63549ea852dc2f7b65816286
Moniqueemh <mohernandez9992@yahoo.com>
2021-01-30 20:34:30 -0800

    added contact component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Container from "react-bootstrap/Container";
+//import Fade from "react-reveal/Fade";
 
 // components
 import MyNavbar, {  } from "./components/my-navbar/mynavbar";
@@ -6,6 +8,7 @@ import MyCarousel from './components/my-carousel/mycarousel.component';
 import TitleMessage from './components/title/title.component'
 import About from './pages/about/about.comp';
 import ProjectTimeline from './components/project-timeline/project-timeline.comp'
+import ContactForm from './pages/contact/contact-form'
 
 import './App.css';
 
@@ -18,6 +21,10 @@ const App = () => {
       {/* about me sect */}
       <About />
       <ProjectTimeline />
+      <Container className="container-box rounded">
+          <hr />
+          <ContactForm />
+      </Container>
     </div>
   );
 }
